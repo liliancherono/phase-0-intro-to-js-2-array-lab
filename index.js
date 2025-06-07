20dbb39d114caa7929af8f9efae1208ca02dc300
Lilian Kirwa <cheronlilian21@gmail.com>
2025-06-08 00:18:17 +0300

    edites .invites.json index.html

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+// Initial cats array
+let cats = ["Milo", "Otis", "Garfield"];
+
+// Destructive functions (modify the original array)
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+// Non-destructive functions (return a new array)
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
+
